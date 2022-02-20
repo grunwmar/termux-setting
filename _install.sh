cc081d38675ad0ec231716379bc451a0a0f8edfa
Martin Grunwald <martingrunwald91@gmail.com>
2022-02-20 18:31:16 +0100

    package installations removed from script

--- _install.sh
+++ _install.sh
@@ -3,13 +3,5 @@
 apt upgrade
 apt update
 
-# add repos
-apt install science-repo
-
-# install some important apps
-apt install python
-apt install zsh
-apt install tmux
-
 # copy source files to its target directories
 bash ./_sources.sh

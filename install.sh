9ed5fd553a6a30609ff6c05052d43a397580650f
Martin Grunwald <martingrunwald91@gmail.com>
2022-02-20 18:05:46 +0100

    typos fixed

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+if ! [ -x "$(command -v git)" ]; then
+  echo 'Error: git is not installed.'
+  echo -n 'Do you want install git? [y/n] '
+  read ANSW
+  if [ $ANSW = "y" ]; then
+  		apt install git
+  	else
+  		exit
+  fi
+fi
+
 mkdir './.git_termux-setting'
-cd './.git_termux-settings'
+cd './.git_termux-setting'
 git clone 'git@github.com:grunwmar/termux-setting.git' './termux-setting'
 cd './termux-setting'
 bash './_install.sh'

5ccd5b790d73cfefe02b1c201817ab9d663107ba
Martin Grunwald <martingrunwald91@gmail.com>
2022-02-22 18:46:12 +0100

    last

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-echo "Packages root-repo and git needs to be installed"
+cd $HOME
 
-mkdir './.git_termux-setting'
-cd './.git_termux-setting'
-git clone 'https://github.com/grunwmar/termux-setting.git' './termux-setting'
-cd './termux-setting'
-bash './_install.sh'
-cd ..
-cd ..
-rm -rf './.git_termux-setting'
+mkdir .tmx_config
+cd .tmx_config
+
---
+++ install_new.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+new_dir () {
+    if [[ ! -d "$1" ]]; then
+        mkdir "$1"
+    fi
+}
+
+
+
+cd $HOME
+echo -n $PWD "--> "
+
+new_dir '.tmx_config'
+cd '.tmx_config'
+
+echo $PWD
+
+BASE='https://raw.githubusercontent.com/grunwmar/termux-setting/main'
+
+
+#curl -s "$BASE/sources.txt"
+
+OLDIFS=$IFS; IFS=$'\n';
+
+new_dir './sources'
+
+echo ""
+
+cat './SOURCES' | while read FILE; do
+
+    if [[ "$FILE" = "#"* || -z "$FILE" ]]; then
+        continue
+    fi
+
+    echo "downloading $FILE"
+    # curl -s "$BASE"/"sources/"$FILE > "./sources/$FILE"
+
+done
+
+
+REGEX='(.*) --> (.*)'
+envsubst < "./DIST" | while read ROW; do
+
+    if [[ "$ROW" = "#"* || -z "$ROW" ]]; then
+        continue
+    fi
+
+    [[ $ROW =~ $REGEX ]] || continue
+    FILE_1="${BASH_REMATCH[1]}"
+    FILE_2="${BASH_REMATCH[2]}"
+
+    cp "$FILE_1" "$FILE_2"
+done
+
+IFS=$OLDIFS
+
+
+
